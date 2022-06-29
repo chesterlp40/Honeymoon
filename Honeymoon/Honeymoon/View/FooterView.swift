b25da38a243d7b2705c9d92abecb933de3fe03d7
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2022-06-29 01:33:51 -0300

    Add Footer view

---
+++ Honeymoon/Honeymoon/View/FooterView.swift
@@ -0,0 +1,60 @@
+//
+//  FooterView.swift
+//  Honeymoon
+//
+//  Created by Ezequiel Rasgido on 29/06/2022.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 42, weight: .light))
+
+            Spacer()
+            
+            Button {
+                print("Success")
+            } label: {
+                Text("Book destination".uppercased())
+                    .font(
+                        .system(
+                            .subheadline,
+                            design: .rounded
+                        )
+                    )
+                    .fontWeight(.heavy)
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 12)
+                    .tint(.pink)
+                    .background(
+                        Capsule().stroke(.pink, lineWidth: 4)
+                    )
+            }
+            
+            Spacer()
+            
+            Image(systemName: "heart.circle")
+                .font(.system(size: 42, weight: .light))
+        }
+        .padding()
+    }
+}
+
+// MARK: - PREVIEW
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(
+                .fixed(
+                    width: 375,
+                    height: 80
+                )
+            )
+    }
+}
