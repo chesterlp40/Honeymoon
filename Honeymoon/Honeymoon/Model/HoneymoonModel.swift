b05b14cfb8dc6e30943078012847e8db4ccd853e
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2022-06-29 01:06:10 -0300

    Add model, data and cardview

---
+++ Honeymoon/Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,16 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Ezequiel Rasgido on 29/06/2022.
+//
+
+import SwiftUI
+
+// MARK: - HONEYMOON MODEL
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
---
+++ Honeymoon/Honeymoon/View/CardView.swift
@@ -0,0 +1,69 @@
+//
+//  CardView.swift
+//  Honeymoon
+//
+//  Created by Ezequiel Rasgido on 29/06/2022.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    // MARK: - HONEYMOON DATA
+    
+    let id = UUID()
+    var destination: Destination
+    
+    var body: some View {
+        Image(destination.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(24)
+            .frame(
+                minWidth: 0,
+                maxWidth: .infinity
+            )
+            .overlay(
+                VStack(alignment: .center, spacing: 12) {
+                    Text(destination.country.uppercased())
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .shadow(radius: 1)
+                        .padding(.horizontal, 18)
+                        .padding(.vertical, 4)
+                        .overlay(
+                            Rectangle()
+                                .fill(.white)
+                                .frame(height: 1),
+                            alignment: .bottom
+                        )
+                    Text(destination.place.uppercased())
+                        .foregroundColor(.black)
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        .frame(minWidth: 85)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 5)
+                        .background(
+                            Capsule()
+                                .fill(.white)
+                        )
+                }
+                    .frame(minWidth: 280)
+                    .padding(.bottom, 50),
+                alignment: .bottom
+            )
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(destination: honeymoonData[1])
+            .previewLayout(
+                .fixed(
+                    width: 375,
+                    height: 600
+                )
+            )
+    }
+}
