5bb9779a9e1549adf9d0b20bbe3646734eaabed2
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2022-06-29 02:12:22 -0300

    Add Guide view

---
+++ Honeymoon/Honeymoon/View/GuideView.swift
@@ -0,0 +1,80 @@
+//
+//  GuideView.swift
+//  Honeymoon
+//
+//  Created by Ezequiel Rasgido on 29/06/2022.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: true) {
+            VStack(alignment: .center, spacing: 20) {
+                HeaderComponent()
+                
+                Spacer(minLength: 10)
+                
+                Text("Get Started!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.pink)
+                
+                Text("Discover and pick the perfect destination for you romantic Honeymoon!")
+                    .lineLimit(nil)
+                    .multilineTextAlignment(.center)
+                
+                Spacer(minLength: 10)
+                
+                VStack(alignment: .leading, spacing: 25) {
+                    GuideComponent(
+                        title: "Like",
+                        subtitle: "Swipe Right",
+                        description: "Do you like this destination? Touch the screen and swipe right. It will be saved to the favorites.",
+                        icon: "heart.circle"
+                    )
+                    GuideComponent(
+                        title: "Dismiss",
+                        subtitle: "Swipe Left",
+                        description: "Would you rather skip this place? Touch the screen and swipe left. You will no longer see it.",
+                        icon: "xmark.circle"
+                    )
+                    GuideComponent(
+                        title: "Book",
+                        subtitle: "Tap the button",
+                        description: "Our selection of honeymoon resorts is perfect setting for you to embark your new life together.",
+                        icon: "checkmark.circle"
+                    )
+                }
+                
+                Spacer(minLength: 10)
+                
+                Button {
+                    print("Tapped")
+                } label: {
+                    Text("Continue".uppercased())
+                        .font(.headline)
+                        .padding()
+                        .frame(minWidth: 0 , maxWidth: .infinity)
+                        .background(Capsule().fill(.pink))
+                        .foregroundColor(.white)
+                }
+
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(.top, 15)
+            .padding(.bottom, 25)
+            .padding(.horizontal, 25)
+        }
+    }
+}
+
+// MARK: - PREVIEW
+struct GuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuideView()
+    }
+}
